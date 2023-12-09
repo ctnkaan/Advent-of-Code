389740bbb1705046d164c6ca232f900b76cd9915
ctnkaan <kaan.taskingenc@gmail.com>
2023-12-09 17:41:45 +0300

    day1 part 1 JS

---
+++ day1/solution.js
@@ -0,0 +1,39 @@
+const fs = require('fs');
+const readline = require('readline');
+const fileStream = fs.createReadStream('./input.txt');
+
+const rl = readline.createInterface({
+    input: fileStream,
+    crlfDelay: Infinity
+});
+
+let answer = 0;
+
+
+
+rl.on('line', (line) => {
+    console.log(`Line: ${line}`);
+
+    let first = null, last = null
+
+    line.split("").forEach((e, i) => {
+        if (e % e === 0 && first === null) {
+            first = e
+        } else if (e % e === 0) {
+            last = e
+        }
+    })
+
+    if (last === null)
+        last = first
+
+    answer += parseInt(first + last)
+    console.log(answer)
+});
+
+
+
+rl.on('close', () => {
+    console.log('Finished reading the file.');
+});
+
